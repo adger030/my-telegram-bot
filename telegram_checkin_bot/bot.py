e8aa453b88cf1d7a42bb4ad89979e84458849511
adger030 <adgerjj@gmail.com>
2025-07-28 00:06:55 +0800

    Update bot.py

--- telegram_checkin_bot/bot.py
+++ telegram_checkin_bot/bot.py
@@ -119,7 +119,7 @@ async def start_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
         "\n"
         "📌 使用说明：\n"
         "1️⃣ 向我发送关键词“#上班打卡”或“#下班打卡”并附带你的IP截图；\n"
-        "2️⃣ 每个关键词每天只能提交一次；\n"
+        "2️⃣ 下班打卡和上班打卡间隔不能超过10小时，否则下班信息不录入；\n"
         "3️⃣ 其他考勤问题请联系部门助理。\n"
         "\n"
         " <a href='https://www.ipaddress.my'>点击这里查看你的IP地址</a>\n"
