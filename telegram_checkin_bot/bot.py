95c481deaf6a1484572d914082cc83c5cb67de90
adger030 <adgerjj@gmail.com>
2025-08-01 17:04:41 +0800

    Update bot.py

--- telegram_checkin_bot/bot.py
+++ telegram_checkin_bot/bot.py
@@ -66,14 +66,13 @@ def has_user_checked_keyword_today_fixed(username, keyword):
     return False
 
 
-
 async def send_welcome(update_or_msg, name):
     welcome_text = (
         f"您好，{name}！\n\n"
         "📌 使用说明：\n"
         "1️⃣ 向机器人发送“#上班打卡”或“#下班打卡”并附带IP截图；\n"
-        "2️⃣ 上下班打卡间隔不能超过12小时，否则下班信息不录入；\n\n"
-        "IP截图标准\n"
+        "2️⃣ 上班打卡需要选择你的班次，即可打卡成功；\n\n”
+        "IP截图标准，包含以下信息\n”
         "① 设备编码：本机序列号\n"
         "② 实时IP：指定网站内显示的IP截图\n"
         "③ 本地时间：电脑任务栏时间截图（需含月、日、时、分）\n\n"
