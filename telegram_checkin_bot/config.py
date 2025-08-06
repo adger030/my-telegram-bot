fc59a9bf18ff611843c2c1354d2b51b9f8012f64
adger030 <adgerjj@gmail.com>
2025-08-06 17:39:43 +0800

    Update config.py

--- telegram_checkin_bot/config.py
+++ telegram_checkin_bot/config.py
@@ -3,6 +3,8 @@ import cloudinary
 
 KEYWORDS = ["#上班打卡", "#下班打卡", "#补卡"]
 ADMIN_IDS = [6337749385, 6447602744, 6396094777, 6725923773, 6420133169, 6566783362]
+# 添加管理员用户名
+ADMIN_USERNAMES = ["Y_MS_KIDLAT"]
 
 TOKEN = os.getenv("TOKEN")
 DATA_DIR = os.getenv("DATA_DIR", "./data")
