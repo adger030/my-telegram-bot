5910b871943ad1a49a6fe9e5fe7c5677716e4d1f
adger030 <adgerjj@gmail.com>
2025-08-11 23:16:57 +0800

    Update bot.py

--- telegram_checkin_bot/bot.py
+++ telegram_checkin_bot/bot.py
@@ -641,11 +641,18 @@ def schedule_daily_reminders():
         # 这里假设我们在 reminders 表加了 chat_id 字段才能发消息
 
 def schedule_send_reminder(chat_id, shift_name):
-    loop = asyncio.get_event_loop()
+    try:
+        loop = asyncio.get_event_loop()
+    except RuntimeError:
+        # 当前线程没有事件循环 -> 创建一个新的
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+    
     if loop.is_running():
         loop.create_task(send_reminder(chat_id, shift_name))
     else:
         loop.run_until_complete(send_reminder(chat_id, shift_name))
+
 
 def restore_reminder_jobs():
     active_reminders = get_active_reminders()
