0c216daa0e7a545397c213fa2abff554e1d9f7cc
adger030 <adgerjj@gmail.com>
2025-08-16 23:28:02 +0800

    Update admin_tools.py

--- admin_tools.py
+++ admin_tools.py
@@ -11,9 +11,10 @@ from sqlalchemy import text
 from dateutil.parser import parse
 
 from db_pg import engine, get_user_logs, get_user_logs_by_name, get_conn, get_user_name, save_message, transfer_user_data
-from config import ADMIN_IDS, BEIJING_TZ, LOGS_PER_PAGE, DATA_DIR, build_and_send_logs
+from config import ADMIN_IDS, BEIJING_TZ, LOGS_PER_PAGE, DATA_DIR
 from export import export_excel
 from shift_manager import get_shift_options, get_shift_times_short
+from logs_utils import build_and_send_logs, send_logs_page
 
 
 # 提取 Cloudinary public_id
