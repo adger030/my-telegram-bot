e95d8a77d60dbaca03237c5e8b8bebe48044ae0e
adger030 <adgerjj@gmail.com>
2025-09-11 21:39:20 +0800

    Update admin_tools.py

--- admin_tools.py
+++ admin_tools.py
@@ -910,7 +910,7 @@ async def commands_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
     text = (
         "📋 指令清单\n\n"
-        "🌟 基础功能\n"
+        "🌟 基础功能（所有人）\n"
         "`/start` - 欢迎信息 & 姓名登记\n"
         "`/mylogs` - 查看本月打卡记录\n"
         "`/lastmonth` - 查看上月打卡记录\n\n"
